4be3621bf76cf1b935d6722daea58abdfd7a062b
TheOreoTM <qwertzybad@gmail.com>
2025-04-13 18:23:13 +0500

    style: Add codeblock style

--- src/components/markdown-content.tsx
+++ src/components/markdown-content.tsx
@@ -20,6 +20,9 @@ export default function MarkdownContent({ content, className }: MarkdownContentP
                 h3: ({ className, ...props }) => (
                     <h3 className={cn("text-xl font-bold mt-6 mb-3", className)} {...props} />
                 ),
+                pre: ({ className, ...props }) => (
+                    <pre className={cn("my-4 p-4 bg-accent/50 rounded-md", className)} {...props} />
+                ),
                 p: ({ className, ...props }) => <p className={cn("my-4", className)} {...props} />,
                 ul: ({ className, ...props }) => <ul className={cn("my-4 list-disc pl-6", className)} {...props} />,
                 li: ({ className, ...props }) => <li className={cn("my-1", className)} {...props} />,
