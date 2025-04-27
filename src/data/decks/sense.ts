4d0f0baa9dde9d4d3133cc085ea0f04b9c15891b
TheOreoTM <qwertzybad@gmail.com>
2025-04-27 18:39:29 +0500

    feat: update sense deck

--- src/data/decks/sense.ts
+++ src/data/decks/sense.ts
@@ -4,27 +4,32 @@ import { Nature } from "@/lib/types";
 
 export const a_hairWhip = new Card({
     title: "Hair Whip",
+    cardMetadata: { nature: Nature.Attack },
     description: ([def, dmg]) => `DEF+${def}. Afterwards, HP-4, DMG ${dmg}+DEF/4.`,
-    effects: [2, 7],
+    effects: [3, 7],
     emoji: CardEmoji.PUNCH,
     cardCategories: ["Attack"],
     effectNames: ["DEF", "DMG"],
     deck: "sense",
+    hpCost: 4,
 });
 
-export const harden = new Card({
-    title: "Harden",
-    description: ([def]) => `HP-2. DEF+${def}`,
-    effects: [2],
-    emoji: CardEmoji.SHIELD,
+export const sharpen = new Card({
+    title: "Sharpen",
+    cardMetadata: { nature: Nature.Util },
+    description: ([def, atk]) => `HP-1. DEF+${def}. ATK+${atk}.`,
+    effects: [2, 2],
+    emoji: CardEmoji.PUNCH,
     cardCategories: ["Utility", "Defense"],
     effectNames: ["DEF"],
     deck: "sense",
+    hpCost: 1,
 });
 
 export const rest = new Card({
     title: "Rest",
-    description: ([hp]) => `DEF-2. Heal ${hp} HP`,
+    cardMetadata: { nature: Nature.Util },
+    description: ([hp]) => `DEF-2 for 2 turns. Heal ${hp} HP`,
     effects: [10],
     emoji: CardEmoji.HEART,
     cardCategories: ["Utility", "Healing"],
@@ -34,17 +39,20 @@ export const rest = new Card({
 
 export const a_pierce = new Card({
     title: "Pierce",
+    cardMetadata: { nature: Nature.Attack },
     description: ([def, dmg]) =>
         `HP-7. DEF+${def}. Afterwards, DMG ${dmg} + (DEF/4). Pierces through 1/4 of the opponent's defense.`,
-    effects: [1, 10],
+    effects: [2, 10],
     emoji: CardEmoji.PUNCH,
     cardCategories: ["Attack"],
     effectNames: ["DEF", "DMG"],
     deck: "sense",
+    hpCost: 7,
 });
 
-const hairBarrier = new Card({
+export const hairBarrier = new Card({
     title: "Hair Barrier",
+    cardMetadata: { nature: Nature.Defense },
     description: ([def]) => `Priority+2. Increases DEF by ${def} until the end of the turn.`,
     effects: [20],
     emoji: CardEmoji.HOURGLASS,
@@ -56,9 +64,9 @@ const hairBarrier = new Card({
 
 export const teaTime = new Card({
     title: "Tea Time",
-    description: ([atk, hp]) =>
-        `ATK+${atk} for both characters. Heal ${hp} for both characters. Gain 1 Tea Time snack.`,
-    effects: [2, 5],
+    cardMetadata: { nature: Nature.Util },
+    description: ([hp]) => `Empower both characters' hands. Heal ${hp} for both characters. Gain 1 Tea Time snack.`,
+    effects: [4],
     tags: { TeaTime: 1 },
     emoji: CardEmoji.HEART,
     cardCategories: ["Utility", "Healing"],
@@ -68,9 +76,10 @@ export const teaTime = new Card({
 
 export const teaParty = new Card({
     title: "Tea Party",
-    description: ([atk, hp]) =>
-        `ATK+${atk} for both characters. Heal ${hp} for both characters. Gain 2 Tea Time snacks.`,
-    effects: [3, 7],
+    cardMetadata: { nature: Nature.Util },
+    description: ([hp]) =>
+        `Empower both characters' hands twice. Heal ${hp} for both characters. Gain 2 Tea Time snacks.`,
+    effects: [7],
     tags: { TeaTime: 2 },
     emoji: CardEmoji.RANDOM,
     cardCategories: ["Utility", "Healing"],
@@ -83,19 +92,20 @@ export const a_piercingDrill = new Card({
     description: ([dmg]) => `HP-12. DMG ${dmg} + DEF/3. Pierces through 1/3 of the opponent's defense.`,
     effects: [14],
     emoji: CardEmoji.PUNCH,
+    cardMetadata: { nature: Nature.Attack, signature: true },
     cardCategories: ["Attack"],
     effectNames: ["DMG"],
     deck: "sense",
-    cardMetadata: { nature: Nature.Attack, signature: true },
+    hpCost: 12,
 });
 
 export const senseDeck = [
     { card: a_hairWhip, count: 2 },
-    { card: harden, count: 2 },
+    { card: sharpen, count: 2 },
     { card: rest, count: 1 },
     { card: a_pierce, count: 2 },
     { card: hairBarrier, count: 3 },
     { card: teaTime, count: 2 },
-    { card: teaParty, count: 1 },
+    { card: teaParty, count: 2 },
     { card: a_piercingDrill, count: 2 },
 ];
